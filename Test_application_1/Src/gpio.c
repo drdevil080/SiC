7ba9a1848790b2e96c5a1c25c17657368a85e3c3
drdevil080 <daniel.rosenkvist@hotmail.com>
2017-05-27 10:00:23 +0200

    "Test Application 1"

--- Test_application_1/Src/gpio.c
+++ Test_application_1/Src/gpio.c
@@ -98,14 +98,14 @@ void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PBPin PBPin */
-  GPIO_InitStruct.Pin = SHDNLinearRegulator_Pin|SHDNSwitchRegulator_Pin;
+  GPIO_InitStruct.Pin = BatteryPowerBus_Pin|SHDNLinearRegulator_Pin|SHDNSwitchRegulator_Pin|LD_G_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PBPin PBPin PBPin */
-  GPIO_InitStruct.Pin = BatteryPowerBus_Pin|PowerPiezo5V_Pin|LD_G_Pin;
+  GPIO_InitStruct.Pin = PowerPiezo5V_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
